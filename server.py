a61964a0bc320ccb36d6ec3408e9698d5bbba30b
vishal-iith <106789351+vishal-iith@users.noreply.github.com>
2023-09-28 21:13:50 +0530

    Update server.py

--- server.py
+++ server.py
@@ -9,45 +9,43 @@ cache_port = 12345
 client_port = 12345
 
 key_dictionary = {
-  'key1': 'val1','key2': 'val2','key3': 'val3','key4': 'val4', 'key5': 'val5','key6': 'val6'
+  'key1': 'val1', 'key2': 'val2', 'key3': 'val3', 'key4': 'val4', 'key5': 'val5', 'key6': 'val6'
 }
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#S = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print ("Successful creation of socket:")
+# S = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("Successful creation of socket:")
 
 s.bind((server_ip, server_port))
-print ("socket binded to %s" %(server_port))
+print("socket binded to %s" % (server_port))
 
 s.listen(5)
-print ("socket is listening")
-c, addr = s.accept()
-print ('Got connection from:', addr)
-
-
-
-# keypart = recv_message.split(" ")[1]
-# key = keypart.split("=")[1]
-
-
-
-
+print("socket is listening")
 
 while True:
-	recv_message = c.recv(1024).decode()
-	#recv_message is in the form of "GET /assignment2?request=key HTTP/1.1"
-	keyp = recv_message.split(" ")
-	if len(keyp)>2 :
-		keypart = keyp[1]
-		key = keypart.split("=")[1]
+    c, addr = s.accept()
+    print('Got connection from:', addr)
 
-		if key in key_dictionary:
-		    #if key is in the data
-			respmsg = "HTTP/1.1 200 OK\r\n\r\n"+" "+key_dictionary[key]
-				#server response in the form of "HTTP/1.1 200 OK <key>"
-		else:
-			respmsg = 'HTTP/1.1 404 NOT FOUND\r\n\r\n'
-			#server response int he form of "HTTP/1.1 404 NOT FOUND"
+    recv_message = c.recv(1024).decode()
+    # recv_message is in the form of "GET /assignment2?request=key HTTP/1.1"
+    keyp = recv_message.split(" ")
+    method = keyp[0]
 
-		c.send(respmsg.encode())
-c.close()
+    if method == "GET":
+        key = keyp[1].split("=")[1]
+        if key in key_dictionary:
+            # if key is in the data
+            respmsg = "HTTP/1.1 200 OK\r\n\r\n" + " " + key_dictionary[key]
+            # server response in the form of "HTTP/1.1 200 OK <key>"
+        else:
+            respmsg = 'HTTP/1.1 404 NOT FOUND\r\n\r\n'
+            # server response in the form of "HTTP/1.1 404 NOT FOUND"
+
+        c.send(respmsg.encode())
+
+    elif method == "PUT":
+        key = keyp[1].split("/")[2]
+        val = keyp[1].split("/")[3]
+        key_dictionary[key] = val
+
+    c.close()
