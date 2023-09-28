70a9af667c1e40aaa4ef6c8a7973a539f2ba47b3
vishal-iith <106789351+vishal-iith@users.noreply.github.com>
2023-09-28 15:09:39 +0000

    Update server.py

--- server.py
+++ server.py
@@ -1,27 +1,53 @@
 import socket
 
-dst_ip = str(input("Enter Server IP: "))
+server_ip = "10.0.1.3"
+cache_ip = "10.0.1.2"
+client_ip = "10.0.1.1"
+
+server_port = 12345
+cache_port = 12345
+client_port = 12345
+
+key_dictionary = {
+  'key1': 'val1','key2': 'val2','key3': 'val3','key4': 'val4', 'key5': 'val5','key6': 'val6'
+}
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #S = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print ("Successful creation of socket:")
-dport = 12345
-s.bind((dst_ip, dport))
-print ("socket binded to %s" %(dport))
+
+s.bind((server_ip, server_port))
+print ("socket binded to %s" %(server_port))
 
 s.listen(5)
 print ("socket is listening")
 c, addr = s.accept()
 print ('Got connection from:', addr)
 
-recv_message = c.recv(1024).decode()
-key_dictionary = {
-  'key1': 'val1','key2': 'val2','key3': 'val3','key4': 'val4', 'key5': 'val5','key6': 'val6'
-}
-while recv_message:
-	if recv_message in key_dictionary:
-		final_responce = key_dictionary[recv_message]
-	else:
-		final_responce = 'Error 404: Not found'
-	c.send(final_responce.encode())
+
+
+# keypart = recv_message.split(" ")[1]
+# key = keypart.split("=")[1]
+
+
+
+
+
+while True:
 	recv_message = c.recv(1024).decode()
+	#recv_message is in the form of "GET /assignment2?request=key HTTP/1.1"
+	keyp = recv_message.split(" ")
+	if len(keyp)>2 :
+		keypart = keyp[1]
+		key = keypart.split("=")[1]
+
+		if key in key_dictionary:
+		    #if key is in the data
+			respmsg = "HTTP/1.1 200 OK\r\n\r\n"+" "+key_dictionary[key]
+				#server response in the form of "HTTP/1.1 200 OK <key>"
+		else:
+			respmsg = 'HTTP/1.1 404 NOT FOUND\r\n\r\n'
+			#server response int he form of "HTTP/1.1 404 NOT FOUND"
+
+		c.send(respmsg.encode())
+c.close()
